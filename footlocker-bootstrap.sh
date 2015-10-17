a5c75652514b2a7679df5b4ab281d7fd24de11d2
Jim McBride <jm@gonkulator.io>
2015-10-17 14:50:44 -0500

    azure updates

--- footlocker-bootstrap.sh
+++ footlocker-bootstrap.sh
@@ -10,22 +10,22 @@ touch therunninghosts
 
 
 vagrant up --provider=$1
-
+vagrant reload
 
 
 mkdir sshconfigs
-vagrant ssh-config cnvm-00 > sshconfigs/cnvm-00-sshconfig
-masterip=$(cat sshconfigs/cnvm-00-sshconfig | grep HostName | awk '{print $2}')
-masteruser=$(cat sshconfigs/cnvm-00-sshconfig | grep User\  | awk '{print $2}')
-mastersshkey=$(cat sshconfigs/cnvm-00-sshconfig | grep IdentityFile | awk '{print $2}')
-masterport=$(cat sshconfigs/cnvm-00-sshconfig | grep Port\  | awk '{print $2}')
+vagrant ssh-config cnvm-host-00 > sshconfigs/cnvm-host-00-sshconfig
+masterip=$(cat sshconfigs/cnvm-host-00-sshconfig | grep HostName | awk '{print $2}')
+masteruser=$(cat sshconfigs/cnvm-host-00-sshconfig | grep User\  | awk '{print $2}')
+mastersshkey=$(cat sshconfigs/cnvm-host-00-sshconfig | grep IdentityFile | awk '{print $2}')
+masterport=$(cat sshconfigs/cnvm-host-00-sshconfig | grep Port\  | awk '{print $2}')
 
 ssh-keyscan -p ${masterport} -t rsa ${masterip} >> ~/.ssh/known_hosts 
-ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo cp id_rsa* /root/.ssh && sudo chown root /root/.ssh/id_rsa*"
+ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes 'sudo cp id_rsa* /root/.ssh && sudo chown root /root/.ssh/id_rsa && sudo chown root /root/.ssh/id_rsa.pub'
 scp -P ${masterport} -i ${mastersshkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes ${masteruser}@${masterip}:./id_rsa* ./thekeys
-echo "Retrieved cnvm-00 ssh-keys"
+echo "Retrieved cnvm-host-00 ssh-keys"
 
-targetnodes=($(cat therunninghosts | grep -v cnvm-00))
+targetnodes=($(cat therunninghosts | grep -v cnvm-host-00))
 
 
 echo "Target nodes are: ${targetnodes[@]}"
@@ -53,11 +53,11 @@ for i in ${targetnodes[@]}; do
 done
 
 echo "Keyscanning master to targets..."
-scp -P ${masterport} -i ${mastersshkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes thekeys/keyscanner.sh ${masteruser}@${masterip}:.
+scp -P ${masterport} -i ${mastersshkey} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes thekeys/*.sh ${masteruser}@${masterip}:.
 ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "sudo ~/keyscanner.sh ${keyscantargets}"
 
 echo "Kicking off Cloud Native VM footlocker builds..."
-footlockertargets=$(cd ./sshconfigs && for i in $(ls | grep -v cnvm-00) ; do cat $i | grep HostName\  | awk '{print $2}' ;done | xargs | sed s/\ /,/g)
+footlockertargets=$(cd ./sshconfigs && for i in $(ls | grep -v cnvm-host-00) ; do cat $i | grep HostName\  | awk '{print $2}' ;done | xargs | sed s/\ /,/g)
 echo "Pulling build container...."
 ssh -p ${masterport} -i ${mastersshkey} ${masteruser}@${masterip} -o LogLevel=FATAL -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes "docker pull gonkulatorlabs/cnvm"
 echo "Building...."
